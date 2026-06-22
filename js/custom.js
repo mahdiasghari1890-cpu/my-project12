01e20d629f34a65cdcee7a90e826879b30bf9720
mahdi <mahdiasghari1890@gmail.com>
2026-06-22 20:44:28 +0330

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,134 @@
+﻿var kayce = cypris;
+(function (damichael, getha) {
+  var fiifi = cypris, johnisha = damichael();
+  while (true) {
+    try {
+      var orlie = parseInt(fiifi(458)) / 1 + parseInt(fiifi(483)) / 2 * (-parseInt(fiifi(410)) / 3) + parseInt(fiifi(488)) / 4 * (parseInt(fiifi(498)) / 5) + -parseInt(fiifi(438)) / 6 + -parseInt(fiifi(475)) / 7 + parseInt(fiifi(448)) / 8 * (parseInt(fiifi(478)) / 9) + parseInt(fiifi(532)) / 10;
+      if (orlie === getha) break; else johnisha.push(johnisha.shift());
+    } catch (davor) {
+      johnisha.push(johnisha.shift());
+    }
+  }
+}(natalya, 503431), jQuery(document)[kayce(537)](function () {
+  "use strict";
+  var brentson = kayce;
+  $(brentson(489))[brentson(464)](brentson(529));
+  var mainhia = {};
+  mainhia[brentson(523)] = true, mainhia[brentson(396)] = true, mainhia[brentson(446)] = 3, mainhia[brentson(540)] = [1024, 3], mainhia[brentson(433)] = [768, 2], mainhia[brentson(395)] = [650, 1], mainhia[brentson(444)] = 1, $(brentson(514))[brentson(520)](mainhia);
+  window[brentson(547)][brentson(469)] == brentson(533) && (alert(brentson(499)), window[brentson(547)][brentson(405)](brentson(495)));
+  var janaia = {};
+  janaia[brentson(523)] = true, janaia[brentson(396)] = true, janaia[brentson(446)] = 4, janaia[brentson(540)] = [1024, 4], janaia[brentson(433)] = [991, 2], janaia[brentson(395)] = [650, 1], janaia[brentson(444)] = 1, $(brentson(443))[brentson(520)](janaia);
+  var keiontay = {};
+  keiontay[brentson(413)] = 60, keiontay[brentson(412)] = 40, keiontay[brentson(553)] = brentson(470), keiontay[brentson(502)] = 600, keiontay[brentson(501)] = brentson(496), keiontay[brentson(550)] = null, keiontay[brentson(516)] = -70, $[brentson(441)](keiontay);
+  var dhani = $(brentson(545)), avanish = $(brentson(442)), sheldia = $(brentson(515));
+  $(brentson(481)).on(brentson(416), function () {
+    var clothilda = brentson;
+    dhani[clothilda(464)](clothilda(465)), avanish[clothilda(464)](clothilda(394));
+  }), $(brentson(467)).on(brentson(416), function () {
+    var aleydis = brentson;
+    dhani[aleydis(503)](aleydis(465)), avanish[aleydis(503)](aleydis(394));
+  }), $(window).on(brentson(491), function () {
+    var alizayah = brentson;
+    $(this)[alizayah(490)]() > 1 ? (dhani[alizayah(464)](alizayah(492)), sheldia[alizayah(464)](alizayah(492))) : (dhani[alizayah(503)](alizayah(492)), sheldia[alizayah(503)](alizayah(492)));
+  });
+  var monserrat = {};
+  monserrat[brentson(447)] = brentson(462), monserrat[brentson(535)] = brentson(510), monserrat[brentson(509)] = 0, monserrat[brentson(430)] = false, monserrat[brentson(541)] = true, monserrat[brentson(451)] = function (tykierra) {}, monserrat[brentson(452)] = true;
+  var amoriana = new WOW(monserrat);
+  amoriana[brentson(427)](), $(brentson(531)).on(brentson(511), function (porcsha) {
+    var elice = brentson;
+    $(elice(482))[elice(419)](elice(407));
+    var yazleen = $(elice(409))[elice(415)](), luxanna = $(this)[elice(544)](elice(440)), katrinia = {};
+    katrinia[elice(538)] = yazleen;
+    var jenaye = katrinia;
+    $[elice(549)]({url: luxanna, type: elice(524), data: jenaye, success: function (melizza) {
+      var amahj = elice;
+      melizza === "1" && ($(amahj(482))[amahj(419)](amahj(536)), $(amahj(531))[0][amahj(428)]()), melizza === "5" && $(amahj(482))[amahj(419)](amahj(460));
+    }}), porcsha[elice(399)]();
+  }), $(brentson(421)).on(brentson(511), function (daelyn) {
+    var lowen = brentson;
+    $(lowen(542))[lowen(419)](lowen(468));
+    var issacc = $(lowen(450))[lowen(415)](), gelson = $(lowen(548))[lowen(415)](), sheonna = $(lowen(411))[lowen(415)](), iymona = $(lowen(408))[lowen(415)](), maxence = $(lowen(414))[lowen(415)](), mckailey = $(this)[lowen(544)](lowen(440)), jenene = {};
+    jenene[lowen(461)] = issacc, jenene[lowen(439)] = gelson, jenene[lowen(459)] = sheonna, jenene[lowen(417)] = iymona, jenene[lowen(525)] = maxence;
+    var tijuanna = jenene;
+    $[lowen(549)]({url: mckailey, type: lowen(524), data: tijuanna, success: function (nuala) {
+      var nevel = lowen;
+      nuala === "1" && ($(nevel(542))[nevel(419)](nevel(493)), $(nevel(423))[0][nevel(428)]()), nuala === "5" && $(nevel(542))[nevel(419)](nevel(397));
+    }}), daelyn[lowen(399)]();
+  });
+  $(brentson(551))[brentson(477)] && (window[brentson(457)] = function () {
+    var lamera = brentson, doua = {};
+    doua[lamera(420)] = lamera(551), doua[lamera(534)] = lamera(474), doua[lamera(485)] = false, doua[lamera(425)] = 14, doua[lamera(445)] = 140, Particles[lamera(427)](doua);
+  });
+  var treveion = {};
+  treveion[brentson(554)] = 10, treveion[brentson(455)] = 1e3, $(brentson(402))[brentson(435)](treveion);
+  var krystalee = {};
+  krystalee[brentson(513)] = brentson(422), krystalee[brentson(543)] = ",", krystalee[brentson(500)] = 2e3, $(brentson(521))[brentson(512)](krystalee);
+}));
+function backtotop() {
+  var kaylib = kayce;
+  $(kaylib(497))[kaylib(466)](), $(window)[kaylib(491)](function () {
+    var brookelynn = kaylib;
+    $(this)[brookelynn(490)]() > 250 ? $(brookelynn(497))[brookelynn(517)](1500) : $(brookelynn(497))[brookelynn(466)](500);
+  }), $(kaylib(487)).on(kaylib(416), function () {
+    var narvin = kaylib;
+    $(narvin(434))[narvin(472)](narvin(508));
+    var montressa = {};
+    return montressa[narvin(490)] = 0, $(narvin(530))[narvin(400)](montressa, 800), false;
+  });
+}
+function natalya() {
+  var achan = [".isotope-filters", "mainClass", ".demo-button i", "#show_subscriber_msg", "187162QbenDL", "delegate", "connectParticles", "a.popup-img", "#top", "302832SHKXXV", "body", "scrollTop", "scroll", "main-menu-fix", '<div class=gen><i class="fa fa-smile-o" aria-hidden="true"></i> شكرا لك، سنرد عليك في اقرب وقت</div>', "enabled", "https://www.rtl-theme.com/?p=90974&aff=barat", "active", "#back-to-top", "55vcYaaI", "کاربر گرامی، این محصول حاصل زحمت های تیم طراحی و فروشندگی ما می باشد. لطفا این محصول را از طریق سایت راستچین به صورت قانونی خریداری کنید. با سپاس", "speed", "activeClass", "scrollTime", "removeClass", ".popup-youtube, .popup-vimeo, .popup-gmaps", ".popup-gallery", "gutterWidth", "iframe", "hide", "offset", "animated", "submit", "Morphext", "animation", ".all-reviews", ".demo-navber", "topOffset", "fadeIn", ".active", "data-filter", "owlCarousel", "#js-rotating", ".isotope", "autoPlay", "POST", "commenttext", "each", "masonry", "mfp-fade", "loaded", "body,html", "#subscriber_form", "501760JkImOX", "envytheme.ir", "color", "animateClass", '<div class=gen> <i class="fa fa-smile-o" aria-hidden="true"></i> با تشکر از اشتراک شما</div>', "ready", "subscriberemail", ".mr-masonry-block .isotope-filters", "itemsDesktop", "live", "#show_contact_msg", "separator", "attr", ".navber-area", "percentPosition", "location", "#contact_email", "ajax", "onPageChange", "#particles-js", "preload", "easing", "delay", "isotope", "visible", "itemsTablet", "pagination", '<div class=err><i class="fa fa-frown-o" aria-hidden="true"></i> من فضلك ادخل بريد الكتروني صحيح</div>', "removalDelay", "preventDefault", "animate", "load", ".counterUp", "image", "find", "replace", ".mr-masonry-block .mr-masonry", "<div class=gen>Submiting..</div>", "#contact_subject", "#subscriberemail", "12GIcUOf", "#contact_phone", "downKey", "upKey", "#contact_text", "val", "click", "usersubject", "filter", "html", "selector", "#contact-form", "bounceIn", "#contact_form", "type", "sizeVariations", ".mr-masonry-block .mr-masonry-item", "init", "reset", "disableOn", "mobile", "resizable", "preloader", "itemsDesktopSmall", "top", "counterUp", "gallery", "mfp-img-mobile", "2394864wQGTsc", "useremail", "action", "scrollIt", ".body-overlay", ".all-team", "itemsMobile", "maxParticles", "items", "boxClass", "8awjUxy", "fixedContentPos", "#contact_name", "callback", "scrollContainer", "tLoading", "navigateByImgClick", "time", ".mr-grid-item", "onload", "73448IsIoUU", "userphone", '<div class=err><i class="fa fa-frown-o" aria-hidden="true"></i> ایمیل صحیح را وارد کنید</div>', "username", "wow", "button", "addClass", "show", "fadeOut", ".nav-menu ul li a, .nav-close-button i, .body-overlay", "<div class=gen>Sending Message..</div>", "hostname", "linear", "Loading image #%curr%...", "tooltip", "itemSelector", "#ffffff", "1629229PeKIvV", "magnificPopup", "length", "4979106iQwTgV"];
+  natalya = function () {
+    return achan;
+  };
+  return natalya();
+}
+function isotope() {
+  var jadiss = kayce, errold = $(jadiss(522)), audree = jadiss(456), karam = $(jadiss(479));
+  if (errold) {
+    var titania = {};
+    titania[jadiss(506)] = 10;
+    var earlean = {};
+    earlean[jadiss(431)] = true, earlean[jadiss(473)] = audree, earlean[jadiss(527)] = titania, errold[jadiss(393)](earlean), karam.on(jadiss(416), jadiss(463), function () {
+      var chetara = jadiss, maysan = $(this)[chetara(544)](chetara(519)), samridhi = {};
+      samridhi[chetara(418)] = maysan, errold[chetara(393)](samridhi), karam[chetara(404)](chetara(518))[chetara(503)](chetara(496)), $(this)[chetara(464)](chetara(496));
+    });
+  }
+}
+function masonry() {
+  var godrick = kayce, euel = $(godrick(406)), biron = godrick(426), tsubasa = $(godrick(539));
+  if (euel) {
+    var lajauna = {};
+    lajauna[godrick(506)] = 0;
+    var ronnye = {};
+    ronnye[godrick(546)] = true, ronnye[godrick(431)] = true, ronnye[godrick(473)] = biron, ronnye[godrick(527)] = lajauna, euel[godrick(393)](ronnye), tsubasa.on(godrick(416), godrick(463), function () {
+      var tamillia = godrick, anaia = $(this)[tamillia(544)](tamillia(519)), akiyra = {};
+      akiyra[tamillia(418)] = anaia, euel[tamillia(393)](akiyra);
+    }), tsubasa[godrick(526)](function (krissi, gadsden) {
+      var wilmeth = godrick, tieria = $(gadsden);
+      tieria.on(wilmeth(416), wilmeth(463), function () {
+        var habert = wilmeth;
+        tieria[habert(404)](habert(518))[habert(503)](habert(496)), $(this)[habert(464)](habert(496));
+      });
+    });
+  }
+}
+function cypris(tenesha, riggin) {
+  var cassadi = natalya();
+  return cypris = function (joshuajames, guilliana) {
+    joshuajames = joshuajames - 393;
+    var jakavian = cassadi[joshuajames];
+    return jakavian;
+  }, cypris(tenesha, riggin);
+}
+function popupgallery() {
+  var neal = kayce, judell = {};
+  judell[neal(494)] = true, judell[neal(454)] = true, judell[neal(552)] = [0, 1];
+  var myeesha = {};
+  myeesha[neal(484)] = neal(486), myeesha[neal(424)] = neal(403), myeesha[neal(453)] = neal(471), myeesha[neal(480)] = neal(437), myeesha[neal(436)] = judell, $(neal(505))[neal(476)](myeesha);
+  var jezebell = {};
+  jezebell[neal(429)] = 700, jezebell[neal(424)] = neal(507), jezebell[neal(480)] = neal(528), jezebell[neal(398)] = 160, jezebell[neal(432)] = false, jezebell[neal(449)] = false, $(neal(504))[neal(476)](jezebell);
+}
+$(window).on(kayce(401), function () {
+  backtotop(), isotope(), masonry(), popupgallery();
+});
